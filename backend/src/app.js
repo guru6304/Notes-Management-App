95a312d358039b0514e56f94ac9f944c349beff6
GURU SWAMY LANKA <LANKAGURUSWAMI@GMAIL.COM>
2026-07-12 16:14:37 +0530

    added frontend url in cors

--- backend/src/app.js
+++ backend/src/app.js
@@ -4,7 +4,15 @@ const errorHandler = require("./middleware/errorHandler");
 const authRouter = require("./routes/authRoutes");
 const noteRoutes = require("./routes/noteRoutes");
 const app= express();
-app.use(cors());
+app.use(
+    cors({
+        origin: [
+            "http://localhost:3000",
+            process.env.FRONTEND_URL
+        ],
+        credentials: true
+    })
+);
 app.use(express.json());
 app.get("/", (req, res) => {
     res.send("Notes Management API is running...");
